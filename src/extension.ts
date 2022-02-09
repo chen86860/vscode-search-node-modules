c48d5151d7d3562ffc676a37e2ae95f10b452b2f
Jack <chen86860@users.noreply.github.com>
2022-02-09 22:54:07 +0800

    feat: lol

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import * as vscode from "vscode";
 import searchNodeModules from "./search";
 
--- src/search.ts
+++ src/search.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
+// eslint-disable-next-line import/no-unresolved
 import * as vscode from "vscode";
 import findChildPackages from "./utils/find-child-packages";
 import findParentModules from "./utils/find-parent-modules";
